dec7cbd4f09d2edb3a8e7e0f11d8ad8db44f265e
tejas <tejas@TEJU_B>
2025-07-29 12:14:35 +0530

    Initial Commit

---
+++ test_project/src/programs_day2/Main.java
@@ -0,0 +1,13 @@
+package programs_day2;
+public class Main {
+	 public static void main(String[] args) {
+	     // Create Author Object
+	     Author author = new Author("J.K. Rowling", "jkrowling@example.com", 'F');
+
+	     // Create Book Object
+	     Book book = new Book("Harry Potter", author, 499.99, 100);
+
+	     // Print Book Details
+	     System.out.println(book.toString());
+	 }
+	}
