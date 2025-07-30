151f81ba792feeee42230bedd9495e39653e82fe
tejas <tejas@TEJU_B>
2025-07-30 09:20:56 +0530

    Initial Commit

--- test_project/src/programs_day2/Exersice29.java
+++ test_project/src/programs_day2/Exersice29.java
@@ -3,7 +3,7 @@ public class Exersice29 {
 	 public static void main(String[] args)
 	 {
 	 String str = "The Quick BroWn FoX!";
-	 // Convert the above string to all lowercase.
+	 // Convert the above string to all 
 	 String lowerStr = str.toLowerCase();
 	 // Display the two strings for comparison.
 	 System.out.println("Original String: " + str);
